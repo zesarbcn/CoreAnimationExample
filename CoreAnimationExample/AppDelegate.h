d6a2eeea111e895a1beba6c067dc33542472ae3c
Cesar Perez <zesarbcn@gmail.com>
2014-09-12 18:29:16 +0200

    Initial Commit

---
+++ CoreAnimationExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreAnimationExample
+//
+//  Created by Cesar Perez Laguna on 12/09/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreAnimationExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreAnimationExample
+//
+//  Created by Cesar Perez Laguna on 12/09/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
